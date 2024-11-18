7096734b98da5abe69f04b53e6b655437a98d0a5
VileKindstrand <vile.kindstrand@elev.ga.ntig.se>
2024-11-18 14:45:14 +0100

    man kan lägga till böcker
    yep

--- app.rb
+++ app.rb
@@ -3,19 +3,42 @@ class App < Sinatra::Base
     def db
         return @db if @db
 
-        @db = SQLite3::Database.new("db/items.sqlite")
+        @db = SQLite3::Database.new("db/todo.sqlite")
         @db.results_as_hash = true
 
         return @db
     end
 
     get '/' do
-        erb(:"/index")
+        redirect("/views")
     end
-    get '/views/new' do
-        erb(:"/index")
-      end
+
+    get '/views' do
+        @items = db.execute('SELECT * FROM todo')
+        erb(:"index")
+    end
+
+    # get '/views/new' do
+    #     erb(:"/new")
+    #     redirect("/views")
+    # end
+
+    post '/views/new' do
+
+        name = params["item_name"]
+        description = params["item_description"]
+        priority = params["item_priority"]
+
+        db.execute("INSERT INTO todo (name, description, priority) VALUES(?,?,?)", [name, description, priority])
+        redirect("/views")
+
+    end
+
+    
     
     
 
 end
+
+
+
--- db/seeder.rb
+++ db/seeder.rb
@@ -7,9 +7,20 @@ class Seeder
     db.execute('DROP TABLE IF EXISTS todo;')
 
     db.execute('CREATE TABLE IF NOT EXISTS todo (
-      id INTEGER PRIMARY KEY,
-      name TEXT
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      description TEXT,
+      priority FLOAT
     );')
+
+    db.execute('INSERT INTO todo (name, description, priority) VALUES ("alfons åberg - den vises sten", "gandalf och garfield slåss i åtta tvåminutersrundor", 9.3)')
+    db.execute('INSERT INTO todo (name, description, priority) VALUES ("1884", "uppfinnandet av hjulet orsakar en kalokainepedemi som tvingar Lars att mysa med råttor", 4.2)')
+    db.execute('INSERT INTO todo (name, description, priority) VALUES ("pippi långa strumpor", "stark ginger", 7.0)')
+
+  end
+
+  def self.populate_tables
+
   end
 
   def self.db
